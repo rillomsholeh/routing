1dc885d9f00a2bc60ae37cdff5b7798b40540d5a
muhammad sholeh <rmsholeh76@gmail.com>
2022-07-23 13:53:20 +0700

    update

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 //allow this app to receive incoming json request
 //Create app.use for express.json here
-app.use(expree.json());
+app.use(express.json());
 
 let todos = [
-    {
-        id: 1,
-        title: "Cuci tangan",
-        isDone: true
-    },
-    {
-        id: 2,
-        title: "Jaga jarak",
-        isDone: false
-    },
-    {
-        id: 3,
-        title: "Jaga Mata",
-        isDone: true
-    },
-]
+  {
+    id: 1,
+    title: "Cuci tangan",
+    isDone: true,
+  },
+  {
+    id: 2,
+    title: "Jaga jarak",
+    isDone: false,
+  },
+  {
+    id: 3,
+    title: "Jaga Mata",
+    isDone: true,
+  },
+];
 
 //GET list route: simply send arr of obj todos on your user screen
 // Create method GET here
 app.get("/todos", (req, res) => {
-    res.send({ data: todos });
-  });
-
-
+  res.send({ data: todos });
+});
 
 //GET detail route: send the todo obj, by received id request params
 // Create method GET by received id here
 app.get("/todo/:id", (req, res) => {
-    const id = req.params.id;
-    const index = id - 1;
-    res.send(todos[index]);
-  });
-
+  const id = req.params.id;
+  const index = id - 1;
+  res.send(todos[index]);
+});
 
 //POST route: receive json body request, from user input, then push to todos array
 // Create method POST here
 app.post("/todo", (req, res) => {
-    todos = [...todos, req.body];
-    res.send({ data: todos });
-  });
-
-
+  todos = [...todos, req.body];
+  res.send({ data: todos });
+});
 
 //PATCH route: receive json body request, from user input, then push to todos array
 //by object id
@@ -60,8 +55,6 @@ app.patch("/todo/:id", (req, res) => {
   res.send({ data: todos[id - 1] });
 });
 
-
-
 //DELETE route: delete the todo obj, by received id request params
 // Create method DELETE here
 app.delete("/todo/:id", (req, res) => {
@@ -70,5 +63,4 @@ app.delete("/todo/:id", (req, res) => {
   res.send({ data: todos });
 });
 
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
